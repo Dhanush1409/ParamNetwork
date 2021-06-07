526dcc6febcfb51783d695f468b39dd74786b5b3
Dhanush1409 <85333652+Dhanush1409@users.noreply.github.com>
2021-06-07 15:05:12 +0530

    Delete MixGroup.js

--- MixGroup.js
+++
@@ -1,7 +0,0 @@
-/*let myString = "Franklin Roosevelt";
-let myRegex = /[\w+]/; // Change this line
-let result = myRegex.test(myString);*/
-let repeatNum = "42 42 42";
-let reRegex = /(\w*)\s\1/; // Change this line
-let result = reRegex.test(repeatNum);
-console.log(result);
