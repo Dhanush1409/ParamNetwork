ffa246d7524288612fabe6a7a3c4cfba242ca0b5
Dhanush1409 <85333652+Dhanush1409@users.noreply.github.com>
2021-06-29 13:39:24 +0530

    Add files via upload

---
+++ Task1/newstruct.js
@@ -0,0 +1,62 @@
+const  department={
+    "Department":"",
+    "Department Supervisor":"",
+    "Today's date":"",
+    "Reviewer Name":"",
+    "Reviewer Title":"",
+    "Last Revised":"",
+    "Department mission":{
+        "Description":"",
+        "Goals":"",
+        "Objectives":""
+    },
+    "GrowthPlan":{
+        "Description":""
+    },
+    "KRA":{
+        "KPI":{
+            "K1":{
+                "K1.1":{
+                    "Description":"",
+                    "Result":[]
+                },
+                "K1.2":{
+                    "Description":"",
+                    "Result":[]
+                },
+                "K1.3":{
+                    "Description":"",
+                    "Result":[]
+                }
+            },
+            "K2":{
+                "K2.1":{
+                    "Description":"",
+                    "Result":[]
+                },
+                "K2.2":{
+                    "Description":"",
+                    "Result":[]
+                },
+                "K2.3":{
+                    "Description":"",
+                    "Result":[]
+                }
+            },
+            "K3":{
+                "K3.1":{
+                    "Description":"",
+                    "Result":[]
+                },
+                "K3.2":{
+                    "Description":"",
+                    "Result":[]
+                },
+                "K3.3":{
+                    "Description":"",
+                    "Result":[]
+                },
+            }
+        }
+    }
+}
