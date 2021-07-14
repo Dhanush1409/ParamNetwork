9ae8524c8b15c9c2d2524485b6a84eeb6c0fe502
Dhanush1409 <85333652+Dhanush1409@users.noreply.github.com>
2021-07-14 21:16:11 +0530

    Delete new directory

--- new/modules/counter.js
+++
@@ -1,7 +0,0 @@
-var mongoose = require('mongoose')
-var schema = new mongoose.Schema([{
-    _id:{type:String},
-    sequence:{type:Number}
-}])
-var model = mongoose.model("counter",schema)
-module.exports = model
